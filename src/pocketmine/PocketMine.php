9d42066e9a5ac1110759d42895f182d3cef3d2d3
Asuna-Yuuki <Wesleyvanneck1234@gmail.com>
2017-09-05 16:18:19 +0200

    start working on the new spigotpe

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -73,7 +73,7 @@ namespace pocketmine {
 	use pocketmine\utils\Utils;
 	use pocketmine\wizard\Installer;
 
-	const VERSION = "1.0.0";
+	const VERSION = "1.1.0 beta";
 	const API_VERSION = "3.0.0-ALPHA5";
 	const CODENAME = "NewBeginnings";
 	const GENISYS_API_VERSION = '2.0.0';
