3b7034b613163012a04c4257ac2e754568d25a88
Fabiansigala <Fabiansigala@yahoo.com>
2017-09-06 07:49:26 -0700

    Update start.cmd

--- start.cmd
+++ start.cmd
@@ -15,7 +15,7 @@ if exist SpigotPE.phar (
 	if exist src\pocketmine\PocketMine.php (
 		set POCKETMINE_FILE=src\pocketmine\PocketMine.php
 	) else (
-		echo "Couldn't find a valid PocketMine-MP installation"
+		echo "Couldn't find a valid SpigotPE installation"
 		pause
 		exit 1
 	)
