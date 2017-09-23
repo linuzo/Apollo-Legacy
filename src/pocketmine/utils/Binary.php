85f231b25066a5e3539cc90fa4251bd284b40c21
wesley van neck <Wesleyvanneck1234@gmail.com>
2017-09-23 18:25:59 +0200

    Update Binary.php

--- src/pocketmine/utils/Binary.php
+++ src/pocketmine/utils/Binary.php
@@ -189,7 +189,7 @@ class Binary {
 	 *
 	 * @return int
 	 */
-	public static function readLShort(boolean $str){
+	public static function readLShort($str){
 		self::checkLength($str, 2);
 
 		return unpack("v", $str)[1];
