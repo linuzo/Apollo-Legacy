e56d5800c7b765781435785b89579dbda7329713
wesley van neck <Wesleyvanneck1234@gmail.com>
2017-09-23 20:22:30 +0200

    hmm

--- src/pocketmine/level/format/SubChunk.php
+++ src/pocketmine/level/format/SubChunk.php
@@ -19,18 +19,18 @@
  *
 */
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace pocketmine\level\format;
 
-class SubChunk{
+class SubChunk implements SubChunkInterface{
 
 	protected $ids;
 	protected $data;
 	protected $blockLight;
 	protected $skyLight;
 
-	private static function assignData(&$target, $data, $length, $value = "\x00"){
+	private static function assignData(&$target, string $data, int $length, string $value = "\x00"){
 		if(strlen($data) !== $length){
 			assert($data === "", "Invalid non-zero length given, expected $length, got " . strlen($data));
 			$target = str_repeat($value, $length);
@@ -46,11 +46,13 @@ class SubChunk{
 		self::assignData($this->blockLight, $blockLight, 2048);
 	}
 
-	public function isEmpty() : bool{
+	public function isEmpty(bool $checkLight = true) : bool{
 		return (
 			substr_count($this->ids, "\x00") === 4096 and
-			substr_count($this->skyLight, "\xff") === 2048 and
-			substr_count($this->blockLight, "\x00") === 2048
+			(!$checkLight or (
+				substr_count($this->skyLight, "\xff") === 2048 and
+				substr_count($this->blockLight, "\x00") === 2048
+			))
 		);
 	}
 
@@ -171,19 +173,19 @@ class SubChunk{
 	}
 
 	public function getBlockIdColumn(int $x, int $z) : string{
-		return substr($this->ids, (($x << 8) | ($z << 4)), 16);
+		return substr($this->ids, ($x << 8) | ($z << 4), 16);
 	}
 
 	public function getBlockDataColumn(int $x, int $z) : string{
-		return substr($this->data, (($x << 7) | ($z << 3)), 8);
+		return substr($this->data, ($x << 7) | ($z << 3), 8);
 	}
 
 	public function getBlockLightColumn(int $x, int $z) : string{
-		return substr($this->blockLight, (($x << 7) | ($z << 3)), 8);
+		return substr($this->blockLight, ($x << 7) | ($z << 3), 8);
 	}
 
-	public function getSkyLightColumn(int $x, int $z) : string{
-		return substr($this->skyLight, (($x << 7) | ($z << 3)), 8);
+	public function getBlockSkyLightColumn(int $x, int $z) : string{
+		return substr($this->skyLight, ($x << 7) | ($z << 3), 8);
 	}
 
 	public function getBlockIdArray() : string{
@@ -196,9 +198,14 @@ class SubChunk{
 		return $this->data;
 	}
 
-	public function getSkyLightArray() : string{
+	public function getBlockSkyLightArray() : string{
 		assert(strlen($this->skyLight) === 2048, "Wrong length of skylight array, expecting 2048 bytes, got " . strlen($this->skyLight));
 		return $this->skyLight;
+	}
+
+	public function setBlockSkyLightArray(string $data){
+		assert(strlen($data) === 2048, "Wrong length of skylight array, expecting 2048 bytes, got " . strlen($data));
+		$this->skyLight = $data;
 	}
 
 	public function getBlockLightArray() : string{
@@ -206,9 +213,13 @@ class SubChunk{
 		return $this->blockLight;
 	}
 
+	public function setBlockLightArray(string $data){
+		assert(strlen($data) === 2048, "Wrong length of light array, expecting 2048 bytes, got " . strlen($data));
+		$this->blockLight = $data;
+	}
+
 	public function networkSerialize() : string{
-		// storage version, ids, data, skylight, blocklight
-		return "\x00" . $this->ids . $this->data . $this->skyLight . $this->blockLight;
+		return "\x00" . $this->ids . $this->data;
 	}
 
 	public function fastSerialize() : string{
@@ -226,5 +237,9 @@ class SubChunk{
 			substr($data, 6144, 2048), //sky light
 			substr($data, 8192, 2048)  //block light
 		);
+	}
+
+	public function __debugInfo(){
+		return [];
 	}
 }
