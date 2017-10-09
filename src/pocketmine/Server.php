a38bd7ac2f16b210f0b7ed7bc60395668f25baa9
wesleyvanneck <wesleyvanneck1012@gmail.com>
2017-10-09 16:09:48 +0200

    damit

--- src/pocketmine/Server.php
+++ src/pocketmine/Server.php
@@ -1782,11 +1782,11 @@ OS: Â§6' . PHP_OS .'Â§f
 
 
 			Generator::addGenerator(Flat::class, "flat");
-			Generator::addGenerator(Normal2::class, "normal");
+			Generator::addGenerator(Normal::class, "normal");
 			Generator::addGenerator(Normal::class, "default");
 			Generator::addGenerator(Nether::class, "hell");
 			Generator::addGenerator(Nether::class, "nether");
-			//Generator::addGenerator(VoidGenerator::class, "void");
+			Generator::addGenerator(VoidGenerator::class, "void");
 			//Generator::addGenerator(Normal2::class, "normal2");
 			Generator::addGenerator(Ender::class, "ender");
 
