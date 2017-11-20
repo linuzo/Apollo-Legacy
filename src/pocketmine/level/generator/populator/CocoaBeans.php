cd37ed2e0f741b8476ecd1fca7000123669fe584
Hatsune Miku <wesleyvanneck1012@gmail.com>
2017-11-20 17:52:10 +0100

    Update CocoaBeans.php

--- src/pocketmine/level/generator/populator/CocoaBeans.php
+++ src/pocketmine/level/generator/populator/CocoaBeans.php
@@ -3,7 +3,7 @@
 CocoaBeans populator
 */
 namespace pocketmine\level\generator\populator;
-use pocketmine\block\BlockFactory;
+use pocketmine\block\Block;
 use pocketmine\level\ChunkManager;
 use pocketmine\utils\Random;
 use pocketmine\level\generator\populator\VariableAmountPopulator;
@@ -19,19 +19,19 @@ class CocoaBeans extends VariableAmountPopulator{
 			$z = $random->nextRange($chunkZ * 16, $chunkZ * 16 + 15);
 			$y = $this->getHighestWorkableBlock($x, $z);
 			if($y !== -1 and $this->canCocoaBeansStay($x, $y, $z)){
-				$this->level->setBlockIdAt($x, $y, $z, BlockFactory::COCOA);
+				$this->level->setBlockIdAt($x, $y, $z, Block::COCOA);
 				$this->level->setBlockDataAt($x, $y, $z, 1);
 			}
 		}
 	}
 	private function canCocoaBeansStay($x, $y, $z){
 		$b = $this->level->getBlockIdAt($x, $y, $z);
-		return $this->level->getBlockIdAt($x, $y, $z) === BlockFactory::WOOD;
+		return $this->level->getBlockIdAt($x, $y, $z) === Block::WOOD;
 	}
 	private function getHighestWorkableBlock($x, $z){
 		for($y = 127; $y >= 0; --$y){
 			$b = $this->level->getBlockIdAt($x, $y, $z);
-			if($b !== BlockFactory::AIR and $b !== BlockFactory::LEAVES and $b !== BlockFactory::LEAVES2 and $b !== BlockFactory::SNOW_LAYER){
+			if($b !== Block::AIR and $b !== Block::LEAVES and $b !== Block::LEAVES2 and $b !== Block::SNOW_LAYER){
 				break;
 			}
 		}
