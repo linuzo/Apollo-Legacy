6a90d2983c65109ed8cdd3883b05970199ee62a4
Hatsune Miku <wesleyvanneck1012@gmail.com>
2017-11-20 17:58:23 +0100

    oops

--- src/pocketmine/level/generator/populator/PopulatorTallGrass.php
+++ src/pocketmine/level/generator/populator/PopulatorTallGrass.php
@@ -21,7 +21,7 @@
 
 namespace pocketmine\level\generator\populator;
 
-use pocketmine\block\BlockFactory;
+use pocketmine\block\Block;
 use pocketmine\level\ChunkManager;
 use pocketmine\utils\Random;
 use pocketmine\block\DoublePlant;
