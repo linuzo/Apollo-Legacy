cd1398d8c1044ef4fdcc05c644fc89ef3e8e593b
wesleyvanneck <wesleyvanneck1012@gmail.com>
2017-12-24 14:45:49 +0100

    we support alpha 10

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -81,7 +81,7 @@ namespace pocketmine {
 
 	const NAME = "Apollo";
 	const VERSION = "1.0dev";
-	const API_VERSION = "3.0.0-ALPHA9";
+	const API_VERSION = "3.0.0-ALPHA10";
 	const CODENAME = "Legacy";
 	const GENISYS_API_VERSION = '2.0.0';
 
